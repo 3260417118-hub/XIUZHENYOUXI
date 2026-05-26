b4bed75eb3022acd2873aad2dd6361f5cda432ae
3260417118-hub <3260417118@qq.com>
2026-05-27 01:20:52 +0800

    add event data model

---
+++ Assets/_Game/Scripts/Data/EventData.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 剧情事件选项数据。
+/// 第一版只支持：显示提示、设置 Flag、增加修为、增加/减少灵石、跳转下一个事件、关闭事件。
+/// </summary>
+[Serializable]
+public class EventOptionData
+{
+    public string text;
+    public string message;
+    public string[] setFlags;
+    public int cultivationGain;
+    public int spiritStoneGain;
+    public string nextEventId;
+    public bool closeEvent;
+}
+
+/// <summary>
+/// 单个剧情事件数据。
+/// 数据来自 Resources/Data/events.json。
+/// </summary>
+[Serializable]
+public class EventData
+{
+    public string id;
+    public string title;
+    public string text;
+    public List<EventOptionData> options = new List<EventOptionData>();
+}
+
+/// <summary>
+/// JsonUtility 不能直接读取最外层数组，所以用这个包装类承接 JSON。
+/// </summary>
+[Serializable]
+public class EventDataList
+{
+    public List<EventData> events = new List<EventData>();
+}
