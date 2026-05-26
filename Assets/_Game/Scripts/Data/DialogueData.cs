fff34dfaf9fcc29bed6b162d6a2eaab3de442e49
3260417118-hub <3260417118@qq.com>
2026-05-27 00:42:55 +0800

    add dialogue data model

---
+++ Assets/_Game/Scripts/Data/DialogueData.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 单个对话选项数据。
+/// action 支持：close、message、dialogue。
+/// </summary>
+[Serializable]
+public class DialogueOptionData
+{
+    public string text;
+    public string action;
+    public string target;
+    public string message;
+}
+
+/// <summary>
+/// 单段对话数据。
+/// 数据来自 Resources/Data/dialogues.json。
+/// </summary>
+[Serializable]
+public class DialogueData
+{
+    public string id;
+    public string speaker;
+    public string text;
+    public List<DialogueOptionData> options = new List<DialogueOptionData>();
+}
+
+/// <summary>
+/// JsonUtility 不能直接读取最外层数组，所以用这个包装类承接 JSON。
+/// </summary>
+[Serializable]
+public class DialogueDataList
+{
+    public List<DialogueData> dialogues = new List<DialogueData>();
+}
